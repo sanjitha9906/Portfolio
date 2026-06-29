ff1fbf54b5efbc9da1fa261f56ffe85028553377
Sanjitha <sanjitha9906@gmail.com>
2026-06-29 09:49:17 +0530

    Use Render port

--- app.js
+++ app.js
@@ -67,6 +67,77 @@ app.get("/contact", (req, res) => {
     res.render("contact");
 });
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
+const express = require("express");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.get('/home', (req, res) => {
+    res.render('index');
+});
+
+app.get("/projects", (req, res) => {
+    res.render("projects");
+});
+
+app.get("/blindspot", (req, res) => {
+  res.render("blindspot");
+});
+
+app.get("/carpool", (req, res) => {
+  res.render("carpool");
+});
+
+app.get("/doorlock", (req, res) => {
+  res.render("doorlock");
+});
+
+app.get("/hackathons", (req, res) => {
+    res.render("hackathons");
+});
+
+app.get('/ossomehacks', (req, res) => {
+    res.render('ossomehacks');
+});
+
+app.get('/hacktrax', (req, res) => {
+    res.render('hacktrax');
+});
+
+app.get('/techsprint', (req, res) => {
+    res.render('techsprint');
+});
+
+app.get('/hackathon10', (req, res) => {
+    res.render('hackathon10');
+});
+
+
+app.get("/courses", (req, res) => {
+    res.render("courses");
+});
+
+app.get("/workshops", (req, res) => {
+    res.render("workshops");
+});
+
+app.get("/about", (req, res) => {
+    res.render("about");
+});
+
+app.get("/contact", (req, res) => {
+    res.render("contact");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
